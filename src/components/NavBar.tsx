b079041e15737489f5a7587894e81779dc49d140
Vroomfrondal <lionscantcode@gmail.com>
2022-11-22 12:38:54 -0600

    [NavBar] Logic/Styling Start
    - minimized padding on title
    - created useNavBar hook to let user navigate between home and favorites page
    - fixed HomePage to route depending on the activePage
    - source code link
    - scroll listener to close modal
    - defaults to HomePage

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react'
+
+function useNavBar() {
+  const [activePage, setActivePage] = useState('Home')
+  const [showNavLinks, setShowNavLinks] = useState(false)
+
+  // close nav menu on item-click
+  useEffect(() => {
+    window.addEventListener('scroll', () => setShowNavLinks(false))
+    setShowNavLinks(false)
+  }, [activePage])
+
+  return {
+    activePage,
+    navBar: (
+      <>
+        <button className="hamburger_container" onClick={() => (showNavLinks ? setShowNavLinks(false) : null)}>
+          <div
+            onClick={() => setShowNavLinks((status) => !status)}
+            className={`hamburger ${showNavLinks ? 'hidden opacity-0' : 'block'}`}
+          >
+            <span className="line w-1"></span>
+            <span className="line w-2"></span>
+            <span className="line w-3"></span>
+          </div>
+        </button>
+
+        <nav className={`nav_bar ${showNavLinks ? 'nav_bar:active' : 'hidden'}`}>
+          <a href="https://www.topherdeleon.com/" target="_blank" rel="noreferrer">
+            <span className="emblem">T</span>
+          </a>
+
+          <button
+            className={`close_container ${showNavLinks ? 'block' : 'hidden'}`}
+            onClick={() => setShowNavLinks((status) => !status)}
+          >
+            <span className="close_nav_button">X</span>
+          </button>
+
+          <hr></hr>
+
+          <div className="links">
+            <button className="home" onClick={() => setActivePage('Home')}>
+              Home
+            </button>
+
+            <button className="favorites" onClick={() => setActivePage('Favorites')}>
+              Favorites
+            </button>
+
+            <a
+              href="https://github.com/Vroomfrondal/Photo-Gallery-React-Tailwind"
+              target="_blank"
+              rel="noreferrer"
+              className="source"
+            >
+              Source
+            </a>
+
+            <a href="https://www.topherdeleon.com/" target="_blank" rel="noreferrer">
+              <span className={`mobile_emblem ${showNavLinks ? 'block' : 'hidden'}`}>T</span>
+            </a>
+          </div>
+        </nav>
+      </>
+    ),
+  }
+}
+
+export default useNavBar
