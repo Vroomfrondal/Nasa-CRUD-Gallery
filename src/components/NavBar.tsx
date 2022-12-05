45c26ffe6193b151f276ebf5aa534614da91a543
Christopher DeLeon <lionscantcode@gmail.com>
2022-12-04 20:23:13 -0600

    refactor: react-router routing (#16)
    - refactored nav bar to handle all client-side-routing with react-router-dom library

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import '../styles/NavBar.css'
 
 function useNavBar() {
@@ -11,61 +12,62 @@ function useNavBar() {
     setShowNavLinks(false)
   }, [activePage])
 
-  return {
-    activePage,
-    navBar: (
-      <>
-        <button className="hamburger_container" onClick={() => (showNavLinks ? setShowNavLinks(false) : null)}>
-          <div
-            onClick={() => setShowNavLinks((status) => !status)}
-            className={`hamburger ${showNavLinks ? 'hidden opacity-0' : 'block'}`}
-          >
-            <span className="line w-1"></span>
-            <span className="line w-2"></span>
-            <span className="line w-3"></span>
-          </div>
+  return (
+    <>
+      <button className="hamburger_container" onClick={() => (showNavLinks ? setShowNavLinks(false) : null)}>
+        <div
+          onClick={() => setShowNavLinks((status) => !status)}
+          className={`hamburger ${showNavLinks ? 'hidden opacity-0' : 'block'}`}
+        >
+          <span className="line w-1"></span>
+          <span className="line w-2"></span>
+          <span className="line w-3"></span>
+        </div>
+      </button>
+
+      <nav className={`nav_bar ${showNavLinks ? 'active_nav_bar' : ''}`}>
+        <a href="https://www.topherdeleon.com/" target="_blank" rel="noreferrer">
+          <img className={`emblem ${showNavLinks ? 'hidden' : 'block'}`} src="media/TopherEmblem.png" />
+        </a>
+
+        <button
+          className={`close_container ${showNavLinks ? 'contents' : 'hidden'}`}
+          onClick={() => setShowNavLinks((status) => !status)}
+        >
+          <span className="close_nav_button">X</span>
         </button>
 
-        <nav className={`nav_bar ${showNavLinks ? 'active_nav_bar' : ''}`}>
-          <a href="https://www.topherdeleon.com/" target="_blank" rel="noreferrer">
-            <img className={`emblem ${showNavLinks ? 'hidden' : 'block'}`} src="media/TopherEmblem.png" />
-          </a>
-
-          <button
-            className={`close_container ${showNavLinks ? 'contents' : 'hidden'}`}
-            onClick={() => setShowNavLinks((status) => !status)}
-          >
-            <span className="close_nav_button">X</span>
-          </button>
-
-          <div className="links">
+        <div className="links">
+          <Link to="/">
             <button className="home" onClick={() => setActivePage('Home')}>
               Home
             </button>
+          </Link>
 
+          <Link to="/Favorites">
             <button className="favorites" onClick={() => setActivePage('Favorites')}>
               Favorites
             </button>
+          </Link>
 
-            <button>
-              <a
-                href="https://github.com/Vroomfrondal/Nasa-CRUD-Gallery"
-                target="_blank"
-                rel="noreferrer"
-                className="source"
-              >
-                Source
-              </a>
-            </button>
-
-            <a href="https://www.topherdeleon.com/" target="_blank" rel="noreferrer">
-              <img className={`mobile_emblem ${showNavLinks ? 'block' : 'hidden'}`} src="media/TopherEmblem.png" />
+          <button>
+            <a
+              href="https://github.com/Vroomfrondal/Nasa-CRUD-Gallery"
+              target="_blank"
+              rel="noreferrer"
+              className="source"
+            >
+              Source
             </a>
-          </div>
-        </nav>
-      </>
-    ),
-  }
+          </button>
+
+          <a href="https://www.topherdeleon.com/" target="_blank" rel="noreferrer">
+            <img className={`mobile_emblem ${showNavLinks ? 'block' : 'hidden'}`} src="media/TopherEmblem.png" />
+          </a>
+        </div>
+      </nav>
+    </>
+  )
 }
 
 export default useNavBar
