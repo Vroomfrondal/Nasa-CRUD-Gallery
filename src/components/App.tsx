65d2e29a408e6e1cef2fbfc353e68762837cad6b
Vroomfrondal <lionscantcode@gmail.com>
2022-12-04 20:22:33 -0600

    refactor: react-router routing
    - refactored nav bar to handle all client-side-routing with react-router-dom library

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,17 +2,20 @@ import React from 'react'
 import useNavBar from './NavBar'
 import HomePage from '../components/Pages/HomePage'
 import Favorites from '../components/Pages/Favorites'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
-  const { navBar, activePage } = useNavBar()
+  const navBar = useNavBar()
 
   return (
     <>
       <div className="banner"></div>
-
       {navBar}
-      {activePage === 'Home' ? <HomePage /> : null}
-      {activePage === 'Favorites' ? <Favorites /> : null}
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Favorites" element={<Favorites />} />
+      </Routes>
     </>
   )
 }
