70ee641469e015bc3687b251cf3a6607e9bcafaf
Vroomfrondal <lionscantcode@gmail.com>
2023-05-10 10:57:29 -0500

    feat: tailwind boilerplate + init
    - installed tailwind using framework guide
    - replaced index.css with tailwind version and moved all base / utility elements
    - added screen media sizes, colors, font, and background image
    - updated readme

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  mode: 'jit',
+  theme: {
+    screens: {
+      sm: '320px',
+      md: '738px',
+      lg: '880px',
+      xl: '1020px',
+      xxl: '2160px',
+    },
+    gridTemplateColumns: {
+      4: 'repeat(4, minmax(10rem, 1fr))',
+    },
+    extend: {
+      colors: {
+        cream: '#ffffff',
+        bg_black: '#121212',
+        pure_black: '#000000',
+        navy_blue: '#141721',
+        hover_opaque: 'rgba(255, 255, 255, 0.3)',
+        modal_blackout: 'rgba(0, 0, 0, 0.7)',
+      },
+      fontFamily: {
+        Raleway: ['Raleway', 'sans-serif'],
+      },
+      backgroundImage: {
+        banner: "url('../../public/media/earth-background.jpg')",
+      },
+    },
+  },
+  plugins: [],
+}
