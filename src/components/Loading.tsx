10b90f108a9d5a096db2f96c5a05c02c1a95bd56
Vroomfrondal <lionscantcode@gmail.com>
2023-05-11 14:17:45 -0500

    refactor: <Loader /> CSS
    - refactored w/ tailwind

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import '../styles/Loading.css'
+// import '../styles/Loading.css'
 
 function Loading() {
-  return <h1 className="Loading">Loading... &nbsp;</h1>
+  return <h1 className="flex justify-center items-center text-2xl font-light pt-2">Loading... &nbsp;</h1>
 }
 
 export default Loading
