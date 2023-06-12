673c5fc54935bc6310f02926d05bd0fd88a0f36c
Vroomfrondal <lionscantcode@gmail.com>
2023-06-12 11:59:15 -0500

    feat: storing language in DB
    - storing current language in DB using i18nextLng

--- i18n.ts
+++ i18n.ts
@@ -15,7 +15,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: 'en',
+    lng: localStorage.getItem('i18nextLng') || 'en',
     fallbackLng: ['en'],
     supportedLngs: ['en', 'es'],
     debug: false,
