3463e68e7a41b902f2aa14f2b7c2fe8551c79cc4
Christopher DeLeon <lionscantcode@gmail.com>
2023-06-22 15:59:28 -0500

    feat: Cypress E2E CRUD Operation Testing (#44)
    * feat: Cypress Boilerplate & First Test
    - installed cypress & cypress testing library
    - added boilerplate files
    - added first test
    
    * feat: Create e2e test
    - wrote cypress test for when user "creates" item in DB by liking image
    
    * feat: 2nd Create Cypress Test
    - second method of creating DB item (liking on hover)
    
    * feat: Cypress Delete Testing
    - wrote 2 tests for when user deletes (unlikes) images from DB
    
    * feat: Cypress Error Control Testing
    - added test for when API errors out or user visits unknown page

--- README.md
+++ README.md
@@ -11,6 +11,20 @@ Built with:
 - [React](https://react.dev/)
 - [Vite](https://vitejs.dev/)
 
+### Libraries & Features
+
+- First PR was the base project complete with all vanilla react and typescript code. Each PR thereafter contains production library integrations by using:
+  - [React Router v7](https://reacttraining.com/blog/react-router-v6-pre) for all client-side routing (navigating pages).
+  - [Ky](https://github.com/sindresorhus/ky) for the API call's fetch.
+  - [React-Query](https://tanstack.com/query/v3/) for all server-state management.
+  - [TailwindCSS](https://tailwindcss.com/) for sanity.
+  - [Twin.Macro](https://github.com/ben-rogerson/twin.macro/blob/master/docs/index.md) & [Styled Components](https://github.com/ben-rogerson/twin.macro) for HTML cleanup and CSS-in-JS with Tailwind.
+  - [React Icons](https://react-icons.github.io/react-icons/)
+  - [Cypress](https://www.cypress.io/) for CRUD operation e2e testing (with [MSW](https://www.npmjs.com/package/msw) to mock API calls) & automation.
+  - [react-i18next](https://react.i18next.com/) - multi-language support (English, Spanish currently).
+  - [Framer Motion](https://www.framer.com/motion/) - animation.
+  - Github actions, [LogRocket](https://ecg.atlassian.net/wiki/spaces/ECGFE/pages/1033207980/Project+2+Simple+CRUD+App#:~:text=PR%20approved%20with-,LogRocket,-integration), and [Vercel](Vercel) - CI, analysis, web-hosting & more.
+
 ## Dev Notes
 
 ### Spinning up locally
@@ -25,22 +39,9 @@ Built with:
 
   ```
   npm run start // spins up project in a local development environment (Vite)
+  npm cypress   // spins up crypress e2e testing environment
   ```
 
 ### NASA API Load Time Optimization
 
 The UI utilizes a custom date-pagination-hook implemented to optimize the API call - because NASA's API takes 4 seconds for 7 days of images; you can't just pass in a huge date range to the API. Each time the last element on page is detected, we update the start and end date, and recall the API.
-
-### Libraries
-
-- First PR was the base project complete with all vanilla react and typescript code. Each PR thereafter contains production library integrations by using:
-  - [React Router v7](https://reacttraining.com/blog/react-router-v6-pre) for all client-side routing (navigating pages).
-  - [Ky](https://github.com/sindresorhus/ky) for the API call's fetch.
-  - [React-Query](https://tanstack.com/query/v3/) for all server-state management.
-  - [TailwindCSS](https://tailwindcss.com/) for sanity.
-  - [Twin.Macro](https://github.com/ben-rogerson/twin.macro/blob/master/docs/index.md) & [Styled Components](https://github.com/ben-rogerson/twin.macro) for HTML cleanup and CSS-in-JS with Tailwind.
-  - [React Icons](https://react-icons.github.io/react-icons/)
-  - [Cypress](https://www.cypress.io/) for CRUD operation testing (with [MSW](https://www.npmjs.com/package/msw) to mock API calls) & automation.
-  - [react-i18next](https://react.i18next.com/) - multi-language support (English, Spanish currently).
-  - [Framer Motion](https://www.framer.com/motion/) - animation.
-  - Github actions, [LogRocket](https://ecg.atlassian.net/wiki/spaces/ECGFE/pages/1033207980/Project+2+Simple+CRUD+App#:~:text=PR%20approved%20with-,LogRocket,-integration), and [Vercel](Vercel) - CI, analysis, web-hosting & more.
---
+++ cypress/e2e/CRUD-Operations.cy.ts
@@ -0,0 +1,116 @@
+// clear liked images on local storage
+before(() => {
+  cy.clearAllCookies()
+  cy.clearAllLocalStorage()
+})
+
+describe('CRUD test suite', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5173/') // https://nasa-photo-gallery.vercel.app/
+  })
+
+  it('(Create) likes an image from inside the cards modal', () => {
+    // Click on the first <Card /> on HomePage
+    cy.findAllByRole('img').eq(2, { timeout: 30000 }).should('exist').click()
+
+    // Assert like button exists & like image
+    cy.findByTestId('like-button-modal').should('exist').click()
+
+    // close modal
+    cy.findByTestId('close-modal').click()
+
+    // assert that our local storage is now populated
+    cy.getAllLocalStorage().then((res) => expect(res['http://127.0.0.1:5173']['nasa-liked-images']).exist)
+  })
+
+  it('(Create) likes an image by hovering a card on the HomePage', () => {
+    // Locate cards, and for first one click the like button without opening modal
+    cy.findAllByTestId('card', { timeout: 30000 })
+      .should('exist')
+      .each((card, index) => {
+        if (index === 0) {
+          cy.wrap(card).findByTestId('like-button').click({ force: true })
+        }
+      })
+
+    // Assert that local storage contains an item
+    cy.getAllLocalStorage().then((res) => {
+      expect(res['http://127.0.0.1:5173']['nasa-liked-images']).to.not.deep.equal([])
+    })
+  })
+
+  it('(Delete) Unlikes an image from inside the cards modal', () => {
+    // Select first card
+    cy.findAllByRole('img').eq(2, { timeout: 30000 }).should('exist').click()
+
+    // like image, then unlike it
+    cy.findByTestId('like-button-modal').should('exist').click().click()
+
+    // Assert that DB has no images inside it
+    cy.getAllLocalStorage().then((res) => {
+      expect(res['http://127.0.0.1:5173']['nasa-liked-images']).to.equal('[]')
+    })
+  })
+
+  it('(Delete) Unlikes an image by hovering on the Favorites Page', () => {
+    // First like an image
+    cy.findAllByTestId('card', { timeout: 30000 })
+      .should('exist')
+      .each((card, index) => {
+        if (index === 0) {
+          cy.wrap(card).findByTestId('like-button').click({ force: true })
+        }
+      })
+
+    // Visit Favorites page
+    cy.visit('http://127.0.0.1:5173/Favorites')
+
+    // Unlike first card by hovering
+    cy.findAllByTestId('card', { timeout: 30000 })
+      .should('exist')
+      .each((card, index) => {
+        if (index === 0) {
+          cy.wrap(card).findByTestId('like-button').click({ force: true })
+        }
+      })
+
+    // Verify its gone in local storage
+    cy.getAllLocalStorage().then((res) => {
+      expect(res['http://127.0.0.1:5173']['nasa-liked-images']).to.equal('[]')
+    })
+  })
+})
+
+describe('Navigation Test Suite', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5173/') // https://nasa-photo-gallery.vercel.app/
+  })
+
+  it('Visits the Home Page', () => {
+    cy.url().should('deep.equal', 'http://127.0.0.1:5173/')
+  })
+
+  it('Visits the Favorites Page', () => {
+    // Go to favorites page
+    cy.findByRole('link', { name: 'Favorites' }).click()
+
+    // assertion
+    cy.url().should('include', '/Favorites')
+
+    // Go back home via home page via unliked photos link
+    cy.findByText('home page').click()
+  })
+
+  it('Tests Language Switcher', () => {
+    cy.findByRole('button', { name: 'ES' }).click()
+    cy.findByRole('button', { name: 'EN' }).click()
+  })
+})
+
+// Simulating an error suite
+describe('Fails gracefully by simulating error and routes to error page', () => {
+  it('Fails gracefully when user visits unknown page', () => {
+    cy.visit('http://127.0.0.1:5173/Error') // https://nasa-photo-gallery.vercel.app/
+    cy.url().should('not.equal', 'http://127.0.0.1:5173/', 'http://127.0.0.1:5173/Favorites')
+  })
+})
