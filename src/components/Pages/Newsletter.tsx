b12a1fc11e3276f5efa86df826ea260d2d2198ee
Vroomfrondal <lionscantcode@gmail.com>
2023-08-15 11:20:20 -0500

    feat: React-hook-form
    - replaced MVP code with react-hook-form integration & removed state
    - added error handling / control

--- src/components/Pages/Newsletter.tsx
+++ src/components/Pages/Newsletter.tsx
@@ -1,41 +1,45 @@
 import React, { useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { FcCheckmark } from 'react-icons/fc'
 import tw, { styled } from 'twin.macro'
 import useSendEmail from '../../hooks/useSendEmail'
 import Title from '../Title'
-import { FcCheckmark } from 'react-icons/fc'
 
-// Convert into custom hook
+type NewsletterInputs = {
+  email: string
+}
+
 function Newsletter() {
-  const [email, setEmail] = useState<string>('')
+  const {
+    register,
+    watch,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<NewsletterInputs>()
+
+  const { t } = useTranslation()
   const [isShowingSentCheckmark, setIsShowingSentCheckmark] = useState(false)
   const [usedEmails, setUsedEmails] = useState<string[]>(JSON.parse(localStorage.getItem('used-emails') || '[]'))
   const [isUserNeedingToSendEmail, setIsUserNeedingToSendEmail] = useState(false)
+  const { data: emailResponse, isFetched: isEmailSent } = useSendEmail(watch('email'), isUserNeedingToSendEmail)
 
-  const { data, isFetched: isEmailSent } = useSendEmail(email, isUserNeedingToSendEmail)
-  const { t } = useTranslation()
-
-  //? Frontend: Anti-spam captcha on submit button press?
-  //* Frontend: clear input after submit & show sent checmark
-  // TODO Frontend: React-form integration
-  // TODO DevOps: Find cheaper method than heroku!
-  // TODO DevOps: Merge PR & Rebase
-
+  // Email response logic (add users to sent-storage, show sent-checkmark)
   useEffect(() => {
-    if (data) {
-      console.log('Response data from email query: ', data)
+    if (emailResponse && watch('email') && watch('email') !== '') {
       setIsUserNeedingToSendEmail(false)
-
-      // Anti-spam: add used emails to state array
-      if (email) setUsedEmails((prev) => [...prev, email])
-      setEmail('')
+      setUsedEmails((prev) => [...prev, watch('email')])
       setIsShowingSentCheckmark(true)
     }
-  }, [data])
+  }, [emailResponse])
 
-  // Anti-Spam: store used emails in local storage
+  // Anti-Spam: store used emails in local storage & clear value
   useEffect(() => {
-    if (usedEmails.length) localStorage.setItem('used-emails', JSON.stringify(usedEmails))
+    if (usedEmails.length) {
+      localStorage.setItem('used-emails', JSON.stringify(usedEmails))
+      setValue('email', '')
+    }
   }, [usedEmails])
 
   // Showing "sent" checkmark after sending email
@@ -46,6 +50,13 @@ function Newsletter() {
       }, 2000)
     }
   }, [isShowingSentCheckmark])
+
+  const onSubmit: SubmitHandler<NewsletterInputs> = (data) => {
+    const { email } = data
+
+    if (!email || !email.includes('@') || isReusedEmail(email)) alert('Please enter a valid email.')
+    else setIsUserNeedingToSendEmail(true)
+  }
 
   const isReusedEmail = (email: string) => {
     const isReusedEmail = usedEmails.some((usedEmail) => {
@@ -61,7 +72,7 @@ function Newsletter() {
       <Title title={t('Newsletter')} />
 
       <ParentContainer>
-        <FormBox onSubmit={(e) => e.preventDefault()}>
+        <FormBox onSubmit={handleSubmit(onSubmit)}>
           <FormHeader>
             <p className="text-2xl font-medium">{t('Enter your email!')}</p>
             <p className="text-xs p-2">
@@ -70,26 +81,19 @@ function Newsletter() {
             <Tip className="hidden md:block">{t('Your email is not stored or used to subscribe.')}</Tip>
           </FormHeader>
 
-          <InputContainer className=" flex">
+          <InputContainer>
             <Inputs>
+              {errors.email && <span className="text-sm text-red-500">Please enter a valid email.</span>}
               <EmailInput
-                isInvalidInput={isReusedEmail(email)}
+                {...register('email', { required: true, minLength: 5 })}
+                isInvalidInput={isReusedEmail(watch('email'))}
                 type="input"
                 placeholder="example@something.com"
                 required={true}
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
               />
 
-              {!isReusedEmail(email) ? (
-                <Submit
-                  type="button"
-                  onClick={() => {
-                    // Form Validation
-                    if (!email || !email.includes('@') || isReusedEmail(email)) alert('Invalid input.')
-                    else setIsUserNeedingToSendEmail(true)
-                  }}
-                >
+              {!isReusedEmail(watch('email')) ? (
+                <Submit type="submit" onClick={() => console.log('Error: ', errors)}>
                   {t('Submit')}
                 </Submit>
               ) : (
@@ -99,7 +103,7 @@ function Newsletter() {
               {isEmailSent && isShowingSentCheckmark ? <FcCheckmark size={25} /> : null}
 
               <Tip className="block md:hidden absolute bottom-1 text-center">
-                {t('Your email is not stored or used to subscribe. test test test')}
+                {t('Your email is not stored or used to subscribe.')}
               </Tip>
             </Inputs>
           </InputContainer>
@@ -120,7 +124,7 @@ const InputContainer = tw.section`flex`
 const FormHeader = tw.section`text-center w-72 md:(relative)`
 const Inputs = tw.section`flex flex-col items-center`
 
-// Form Elements
+// Form Inputs
 const Submit = tw.button`border border-cream px-2 mt-1`
 const Submitted = tw.span`text-sm`
 const Tip = tw.p`text-xs`
